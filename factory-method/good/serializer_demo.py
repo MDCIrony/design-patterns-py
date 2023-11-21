39c2e5e720f0f087a636304906a160624d31d1c5
MDCIrony <miguel.acastillodiaz@outlook.es>
2023-11-21 16:26:33 -0500

    Refactored code for basic implementation of factorymethod

---
+++ factory-method/good/serializer_demo.py
@@ -0,0 +1,65 @@
+import json
+import xml.etree.ElementTree as et
+from abc import ABC, abstractmethod
+
+from bad.serializer_demo import Song
+
+class SerializerInterface(ABC):
+    @abstractmethod
+    def _serialize(self, song: Song) -> str:
+        raise NotImplementedError('serialize() not implemented')
+
+
+class JsonSerializer(SerializerInterface):
+    def _serialize(self, song: Song) -> str:
+        payload = {
+            'id': song.song_id,
+            'title': song.title,
+            'artist': song.artist
+        }
+        return json.dumps(payload)
+
+class XmlSerializer(SerializerInterface):
+    def _serialize(self, song: Song) -> str:
+        song_element = et.Element('song', attrib={'id': song.song_id})
+        title = et.SubElement(song_element, 'title')
+        title.text = song.title
+        artist = et.SubElement(song_element, 'artist')
+        artist.text = song.artist
+        return et.tostring(song_element, encoding='unicode')
+
+
+# Creator function
+def serialize(song: Song , serializer: SerializerInterface) -> str:
+    return serializer._serialize(song)
+
+
+class SongSerializerV1:
+    def serialize(self, song, format):
+        serializer = self._get_serializer(format)
+        print(serializer)
+        return serializer(song)
+
+    def _get_serializer(self, format):
+        if format == 'JSON':
+            return self._serialize_to_json
+        elif format == 'XML':
+            return self._serialize_to_xml
+        else:
+            raise ValueError(format)
+
+    def _serialize_to_json(self, song):
+        payload = {
+            'id': song.song_id,
+            'title': song.title,
+            'artist': song.artist
+        }
+        return json.dumps(payload)
+
+    def _serialize_to_xml(self, song):
+        song_element = et.Element('song', attrib={'id': song.song_id})
+        title = et.SubElement(song_element, 'title')
+        title.text = song.title
+        artist = et.SubElement(song_element, 'artist')
+        artist.text = song.artist
+        return et.tostring(song_element, encoding='unicode')
--- factory-method/main.py
+++ factory-method/main.py
@@ -1,23 +1,30 @@
-import bad.serializer_demo as sd
+import bad.serializer_demo as bs
+import good.serializer_demo as gs
 
 
-song = sd.Song('1', 'Water of Love', 'Dire Straits')
-serializer = sd.SongSerializer() # -> New serializer object
 
-json_song = serializer.serialize(song, 'JSON')
-xml_song = serializer.serialize(song, 'XML')
 
 if __name__ == "__main__":
-    print(json_song)
-    print(xml_song)
+    mode = "GOOD"
+    song = bs.Song('1', 'Water of Love', 'Dire Straits')
+    jsonSerializer = gs.JsonSerializer()
+    xmlSerializer = gs.XmlSerializer()
     
-    
-    # If we try to serialize the song in YAML format, we get a ValueError:
-    try:
-        serializer.serialize(song, 'YAML')
-    except ValueError as e:
-        print(e)
-
+    if mode == "BAD":
+        serializer = bs.SongSerializer() # -> New serializer object
+        print(serializer.serialize(song, 'JSON'))
+        print(serializer.serialize(song, 'XML'))   
+        
+        # If we try to serialize the song in YAML format, we get a ValueError:
+        try:
+            serializer.serialize(song, 'YAML')
+        except ValueError as e:
+            print(e)
+    else:
+        print(gs.serialize(song, jsonSerializer))
+        # serializer = gs.SongSerializerV1()
+        # print(serializer.serialize(song, 'JSON'))
+        
     
 # The problem with this approach is that we have to 
 # modify the SongSerializer class every time we want to add a new format, broken the Open-Closed Principle.
